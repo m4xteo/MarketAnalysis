1d604d15a8417377e2f32acf249d4179e50defba
m4xteo <117918420+m4xteo@users.noreply.github.com>
2024-04-24 01:04:17 +1200

    Update marketAnalysis.py
    added crypto integration

--- marketAnalysis.py
+++ marketAnalysis.py
@@ -26,11 +26,17 @@ import pandas
 api_key = "TQ7srJMzwQvs5TU1eQiBWndnFCk8GwYF"
 stockTicker ="AAPL"
 
-# api links
+# api links stocks
 api_fullquote = "https://financialmodelingprep.com/api/v3/quote/" + stockTicker + "?apikey="
 api_otcquote = "https://financialmodelingprep.com/api/v3/otc/real-time-price/BATRB?apikey="
 api_exsymbols = "https://financialmodelingprep.com/api/v3/symbol/"+ stockTicker + "?apikey="
 api_profile =  "https://financialmodelingprep.com/api/v3/profile/"+ stockTicker + "?apikey="
+
+#https://docs.coincap.io/#61e708a8-8876-4fb2-a418-86f12f308978
+#api links crypto
+crypto_ticker = "bitcoin"
+crpyto_interval = "d1" #day
+crypto_api_historical = "https://api.coincap.io/v2/assets/" + crypto_ticker +"/history?interval=" + crpyto_interval
 
 
 #functions
@@ -40,6 +46,7 @@ def printLogo():
 ┛ ┗┗┻┛ ┛┗┗ ┗  ┛┗┛┗┗┻┗┗┫┛┗┛
                       ┛   
 # Developed by Matt (m4xteo) and Karl (nok-si)
+For crypto and stock markets.
  
 """)
 
@@ -49,14 +56,19 @@ def main():
 	#plt.plot(grabData(api_exsymbols, api_key))
 	#plt.show()
 
-	rawData = grabData(api_fullquote, api_key)
+	rawData = grabData(crypto_api_historical, "")
 	#jsonObject = json.loads(rawData)
 	#closingPrice = (jsonObject["price"])
 
-	price = rawData[0]["price"]
-	dayLow = rawData[0]["dayLow"]
-	print(rawData[0]["price"])
+	#price = rawData[0]["price"]
+	#dayLow = rawData[0]["dayLow"]
+	#print(rawData[0]["price"])
 
+
+
+def showGraph(ticker, date, timestamp):
+	
+	return x
 
 # Function to grab the data (raw)
 def grabData(apiLink, apiKey): 
@@ -81,7 +93,6 @@ def _RSI(stock, avg):
 
 def _MCD_(stock, avg):
 	return x 
-
 
 
 if __name__ == "__main__":
